164f0d7ba1055915938aa3ddd5b0d362d7aaf593
Ben Adamski <benjadamski@gmail.com>
2017-11-20 12:48:06 -0500

    Remove linter-ruby from apm installation list
    - Removes linter-ruby from installed atom packages in atom.sh script.
    - Developers should be using rubocop instead of linter-ruby.
     - Rubocop is deeply opinionated but they should be using and reading those
    messages during WDI.
    - Close #27

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -4,7 +4,7 @@ if which apm; then
   apm install aligner aligner-scss aligner-css aligner-javascript aligner-ruby atom-beautify \
   editorconfig linter-ui-default busy-signal esformatter fixmyjs git-diff-details git-history \
   git-plus language-markdown less-than-slash linter linter-csslint linter-eslint \
-  linter-markdown linter-rubocop linter-ruby linter-tidy \
+  linter-markdown linter-rubocop linter-tidy \
   markdown-writer sort-lines language-ember-htmlbars intentions linter-sass-lint \
   standardjs-snippets ruby-block
 
