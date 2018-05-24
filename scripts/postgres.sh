c0e489707821db779ab8614a75b863138c1658a5
ArjunRayGA <aray@brandeis.edu>
2018-05-24 01:18:14 -0400

    postgres install for non-mac required start of service first, sleep for service to start, then user and db creation

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -15,12 +15,15 @@ if [[ $(uname -s) = 'Darwin' ]]; then
 else
   # install postgres and build dependency
   sudo apt-get install -y postgresql libpq-dev
+  # start postgres server
+  sudo service postgresql start
+  # wait a few seconds to allow the service to start
+  sleep 3s
   # create user in postgres with name of current system user
   sudo -u postgres createuser "$(whoami)" -s
   # create database with name of current system user
   sudo -u postgres createdb "$(whoami)"
-  # start postgres server
-  sudo service postgresql start
+
 fi
 
 # Resource bashrc
