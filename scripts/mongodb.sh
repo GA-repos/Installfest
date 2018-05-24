e8ce5b25bd3799aaa2082ac0b7ad32ee58ecefd8
ArjunRayGA <aray@brandeis.edu>
2018-05-24 05:41:29 -0400

    WSL does not have upstart so mongodb-org service does not run, instead use mongodb package and run mongodb service and mongod daemon

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -3,19 +3,33 @@
 set -e
 
 if [[ $(uname -s) = 'Darwin' ]]; then
+
   brew install mongodb
 
   brew services restart mongodb
+
 else
+
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
   echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
   sudo apt update
 
-  sudo apt install -y mongodb-org
+  if grep -q Microsoft /proc/version; then
 
-  sudo service mongod start
+    sudo apt-get install -y mongodb
+    
+    sudo service mongodb start 
+    
+    sudo mongod
+  
+  else
+
+    sudo apt install -y mongodb-org
+
+    sudo service mongod start
+  fi
 fi
 
 echo "MongoDB appears to have installed succesfully. Follow the steps in the instructions to verify."
