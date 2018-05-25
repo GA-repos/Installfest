723dde3fb44fd24ff2cf06906789cd2e6b9985bd
ArjunRayGA <aray@brandeis.edu>
2018-05-25 11:18:53 -0400

    Startup script run on Windows 10 will fail because Windows will block execution unless user has already explicitly tried to execute script. Therefore, instead of putting what was previously ubuntu_setup.bat in the Startup foler, got rid of script to move ubuntu_setup.bat to Startup folder and renamed ubuntu_setup.bat to ubuntu_install.bat. From now on, user will download script and run it until it says its complete and deletes itself.

--- scripts/wsl/ubuntu_install.bat
+++ scripts/wsl/ubuntu_install.bat
@@ -1,3 +1,23 @@
 @echo OFF
-xcopy /Y ubuntu_startup.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-call "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ubuntu_startup.bat"
+
+REM check if bash is installed
+bash --version 1>nul 2>nul
+if not %errorlevel% == 0 (
+    
+    REM check if lxrun is installed
+    lxrun 1>nul 2>nul
+    if %errorlevel% == 9009 (
+
+        REM turn on wsl
+        powershell -command Start-Process powershell -Verb runAs -Argument 'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux'
+    ) else (
+
+        REM install Ubuntu bash using lxrun
+        powershell -command Start-Process powershell -Verb runAs -Argument 'lxrun /install'
+    )
+) else (
+
+    set /p DUMMY="Installation complete! Open Bash on Ubuntu for Windows from your Start Menu. If you have trouble finding/running it, ask an Instructor for help. Press Enter to close"
+    REM delete self
+    del "%~f0"
+)
