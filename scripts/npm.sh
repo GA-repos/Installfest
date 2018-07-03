aedf6207d8f616fac0e6fe8aa7af959ac126bb79
deconstructionalism <deconstructionalism@gmail.com>
2018-07-03 11:34:06 -0400

    Added wsl-open, to allow opening Win10 apps from bash
    On mac, we use the `open` command, but on WSL, you need to install
    and use `wsl-open` instead. In the `unix-cli` lesson, we specifically
    instruct opening a text file with `open` so we probably need to update
    that lesson to inlcude `wsl-open` for WSL, and `xdg-open` for Ubuntu.

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # install these npm packages globally
 npm install --global npm jsonlint eslint grunt-cli remark-lint phantomjs-prebuilt
+# if Ubunutu Linux on WSL for Windows 10
+if grep -q Microsoft /proc/version; then
+    npm install --global wsl-open
+fi
