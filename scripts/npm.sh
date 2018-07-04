22441aeefeb442c2c7c14fa05237f05255d4ed8c
deconstructionalism <deconstructionalism@gmail.com>
2018-07-03 20:38:49 -0400

    Aliased `wsl-open` to `open`.
    This allows for syntax for opening files on WSL  to match nearly exactly
     the usage for Macs.

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -4,4 +4,7 @@ npm install --global npm jsonlint eslint grunt-cli remark-lint phantomjs-prebuil
 # if Ubunutu Linux on WSL for Windows 10
 if grep -q Microsoft /proc/version; then
     npm install --global wsl-open
+
+    # alias wsl-open to `open` so it works just like on Mac
+    echo "alias open='wsl-open'" >> ~/.bash_profile
 fi
