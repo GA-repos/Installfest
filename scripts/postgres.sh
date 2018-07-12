a127dfaf4943f2f3ba44103d3573681972cc25ed
deconstructionalism <deconstructionalism@gmail.com>
2018-07-12 13:49:39 -0400

    PSQL needs template1 to be Unicode encoding to work with Rails.
    Windows sets up template1 as an 'ASCII_SQL' encoding which won't work
    with rails. This addition drops that table and re-makes it with a
    'Unicode' encoding.

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -23,7 +23,15 @@ else
   sudo -u postgres createuser "$(whoami)" -s
   # create database with name of current system user
   sudo -u postgres createdb "$(whoami)"
+fi
 
+if grep -q Microsoft /proc/version 1>/dev/null 2>/dev/null; then
+  # windows sets up template1 as an 'ASCII_SQL' encoding which won't work with rails
+  # here, we change template1 to 'UNICODE' encoding
+  psql -c "UPDATE pg_database SET datistemplate=FALSE WHERE datname='template1';" &> /dev/null
+  psql -c "DROP DATABASE template1;" &> /dev/null
+  psql -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';" &> /dev/null
+  psql -c "UPDATE pg_database SET datistemplate=TRUE WHERE datname='template1';" &> /dev/null
 fi
 
 # Resource bashrc
