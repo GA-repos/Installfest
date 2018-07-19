47d499ce61e26c0030cb188667460ac17dcfe1b1
MicFin <mrfinneran@gmail.com>
2018-07-19 15:53:52 -0400

    Remove linter style lint

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -14,7 +14,7 @@ fi
 if which apm; then
   apm install aligner aligner-scss aligner-css aligner-javascript aligner-ruby atom-beautify \
   editorconfig linter-ui-default busy-signal esformatter fixmyjs git-diff-details git-history \
-  git-plus language-markdown less-than-slash linter linter-handlebars linter-stylelint linter-eslint \
+  git-plus language-markdown less-than-slash linter linter-handlebars linter-eslint \
   linter-markdown linter-rubocop linter-tidy \
   markdown-writer sort-lines language-ember-htmlbars intentions linter-sass-lint \
   standardjs-snippets ruby-block open-in-browser
