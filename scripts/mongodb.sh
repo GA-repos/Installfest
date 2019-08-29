409261dc18232e0aa726affcb477df4f18f91602
Erica Salling <esalling23@gmail.com>
2019-08-29 12:08:50 -0400

    Update MongoDB Version
    Update mongodb to version 4.0.3
    
    Update install and status commands for Ubuntu to use `systemctl` so we
    can remove the old keyserver steps. Using systemctl and installing
    `mongodb` rather than `mongodb-org` removes the need for those steps.
    
    Tested this on DigitalOcean ubuntu server.
    
    Also change the markdown file to use the correct version.

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -7,15 +7,11 @@ if [[ $(uname -s) = 'Darwin' ]]; then
 
   brew services restart mongodb
 else
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-
-  echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
   sudo apt update
 
-  sudo apt install -y mongodb-org
+  sudo apt install -y mongodb
 
-  sudo service mongod start
+  sudo systemctl start mongodb
 fi
 
 echo "MongoDB appears to have installed succesfully. Follow the steps in the instructions to verify."
